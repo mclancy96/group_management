f21e00ae0d40ae25abc8ee548a411219065901c4
Mike Clancy <mclancy96@gmail.com>
2024-12-01 00:10:15 -0500

    basic setup

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,18 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import React from "react";
+import ReactDOM from "react-dom";
+import "@playbook/pb_kits/pb_kits.js";
+const components = require.context("./components", true, /\.jsx?$/);
+import 'playbook-ui/dist/playbook-rails.js';
+
+components.keys().forEach((fileName) => {
+    const componentName = fileName
+        .split("/")
+        .pop()
+        .replace(/\.\w+$/, "");
+
+    const Component = components(fileName).default;
+
+    document.querySelectorAll(`[data-react-component="${componentName}"]`).forEach((el) => {
+        ReactDOM.render(<Component {...(el.dataset.props ? JSON.parse(el.dataset.props) : {})} />, el);
+    });
+});
